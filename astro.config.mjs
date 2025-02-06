7e9658c964bd5a9307024fb9998a3d593a252424
Pablo Ivan Moraga Sandoval <pmoraga.e@cocha.com>
2025-02-06 14:32:39 -0300

    Actualizar configuración de Astro para cambiar el directorio de salida a './dist' y ajustar el diseño del componente de contacto

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: 'https://ketbome.github.io/inventarioccp',
-  base: '/',
-  output: 'static',
-  outDir: './docs', // GitHub Pages busca en /docs por defecto
+  outDir: './dist', // GitHub Pages busca en /docs por defecto
   vite: {
     plugins: [tailwindcss()],
   },
