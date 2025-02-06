2607bcb5cec5b9b4d0caf4f640be75248d4476ca
Pablo Ivan Moraga Sandoval <pmoraga.e@cocha.com>
2025-02-06 13:09:57 -0300

    Actualizar la configuración de Astro para eliminar la base del sitio y ajustar el diseño del componente de contacto

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ketbome.github.io',
-  base: '/inventarioccp',
+  base: '',
   vite: {
     plugins: [tailwindcss()],
   },
